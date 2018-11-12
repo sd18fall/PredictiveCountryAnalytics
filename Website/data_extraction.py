49c076fb077189556f27561b58a186c05d395b4d
sballantyne <sara.ballantyne@students.olin.edu>
2018-11-11 21:46:05 -0500

    Updated website with 3 working sliders and a drop down menu

---
+++ Website/data_extraction.py
@@ -0,0 +1,22 @@
+""" Extracts data from csv files to be displayed on the website along with
+coefficients """
+
+import pygame
+
+__author__ = "Sara Ballantyne"
+__version__ = '0.0.1'
+
+def data_extraction():
+    """Pulls data from csv columns as needed for website to pass into javascript --> html
+    >>> data_extraction()
+
+    """
+    pass
+
+
+def coefficients_calc():
+    """ Calculates a coefficient for each country based on weights given by JS
+    sliders and dropdown menus (This might be best calculated in the javascript itself)
+
+    """
+    pass
--- Website/script.js
+++ Website/script.js
@@ -3130,11 +3130,12 @@
 });
 
 /*SARA LOOK HERE*/
-var slider = document.getElementById("myRange");
-var output = document.getElementById("demo");
-  output.innerHTML = slider.value; // Display the default slider value
-
-  // Update the current slider value (each time you drag the slider handle)
-  slider.oninput = function() {
-      output.innerHTML = this.value;
-  }
+//
+// var slider = document.getElementById("myRange");
+// var output = document.getElementById("demo");
+//   output.innerHTML = slider.value; // Display the default slider value
+//
+//   // Update the current slider value (each time you drag the slider handle)
+//   slider.oninput = function() {
+//       output.innerHTML = this.value;
+//   }
